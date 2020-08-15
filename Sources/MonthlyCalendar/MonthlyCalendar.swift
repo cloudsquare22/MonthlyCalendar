f1235a205d70992794477b2504577666e33a8e36
shi-n <shi-n_xyz@cloudsquare.jp>
2020-08-15 21:05:49 +0900

    1st

--- Sources/MonthlyCalendar/MonthlyCalendar.swift
+++ Sources/MonthlyCalendar/MonthlyCalendar.swift
@@ -1,3 +1,37 @@
+import Foundation
+
 struct MonthlyCalendar {
-    var text = "Hello, World!"
+    var year = 0
+    var month = 0
+    var dayCount = 0
+    var days = [[Int]]()
+    
+    init(day: Date) {
+        days = [[Int]](repeating: [Int](repeating: 0, count: 7), count: 1)
+        let dayDateComponents = Calendar.current.dateComponents(in: .current, from: day)
+        self.year = dayDateComponents.year!
+        self.month = dayDateComponents.month!
+        var firstDateComponents = dayDateComponents
+        firstDateComponents.day = 1
+        firstDateComponents = Calendar.current.dateComponents(in: .current, from: Calendar.current.date(from: firstDateComponents)!)
+        var countDateComponents = DateComponents()
+        countDateComponents.year = firstDateComponents.year
+        countDateComponents.month = firstDateComponents.month! + 1
+        countDateComponents.day = 0
+        self.dayCount = Calendar.current.component(.day, from: Calendar.current.date(from: countDateComponents)!)
+        let weekday = firstDateComponents.weekday!
+        var weekdayIndex = weekday - 1 == 0 ? 6 : weekday - 2
+        var weekIndex = 0
+        for day in 1...self.dayCount {
+            days[weekIndex][weekdayIndex] = day
+            if weekdayIndex + 1 > 6 {
+                weekdayIndex = 0
+                weekIndex = weekIndex + 1
+                days.append([Int](repeating: 0, count: 7))
+            }
+            else {
+                weekdayIndex = weekdayIndex + 1
+            }
+        }
+    }
 }
--- Tests/MonthlyCalendarTests/MonthlyCalendarTests.swift
+++ Tests/MonthlyCalendarTests/MonthlyCalendarTests.swift
@@ -2,14 +2,31 @@ import XCTest
 @testable import MonthlyCalendar
 
 final class MonthlyCalendarTests: XCTestCase {
-    func testExample() {
+    func testCurrentAndNext() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MonthlyCalendar().text, "Hello, World!")
+        let calendarCurrent = MonthlyCalendar(day: Date())
+        print(calendarCurrent.year)
+        print(calendarCurrent.month)
+        print(calendarCurrent.days)
+        print(calendarCurrent.dayCount)
+        XCTAssertNotEqual(calendarCurrent.year, 0)
+        XCTAssertNotEqual(calendarCurrent.month, 0)
+        XCTAssertNotNil(calendarCurrent.days)
+
+        let nextMonth = Calendar.current.date(byAdding: .month, value: 1, to: Date())
+        let calendarNext = MonthlyCalendar(day: nextMonth!)
+        print(calendarNext.year)
+        print(calendarNext.month)
+        print(calendarNext.days)
+        print(calendarNext.dayCount)
+        XCTAssertNotEqual(calendarNext.year, 0)
+        XCTAssertNotEqual(calendarNext.month, 0)
+        XCTAssertNotNil(calendarNext.days)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testCurrentAndNext", testCurrentAndNext),
     ]
 }
